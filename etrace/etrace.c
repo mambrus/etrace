4c189f3da1865cfc08f2196f8f0909b8bba117c2
Michael Ambrus <michael.ambrus@gmail.com>
2015-02-25 14:50:23 +0100

    logging: Introduce Android & Bladerf style macros
    Use the Android version for transparency. It's also much less typing.

--- etrace/etrace.c
+++ etrace/etrace.c
@@ -4,7 +4,13 @@
 
 int main(int argc, char ** argv) {
 	printf("Hello world: v%s \n", VERSION );
-	log_set_verbosity(LOG_LEVEL_WARNING);
-	log_write(LOG_LEVEL_WARNING, "Hello helllo %d\n", 1);
+	//log_set_verbosity(LOG_LEVEL_VERBOSE);
+	LOGV("Hello [VERBOSE] %d\n",	LOG_LEVEL_VERBOSE);
+	LOGD("Hello [DEBUG] %d\n",		LOG_LEVEL_DEBUG);
+	LOGI("Hello [INFO] %d\n",		LOG_LEVEL_INFO);
+	LOGW("Hello [WARNING] %d\n",	LOG_LEVEL_WARNING);
+	LOGE("Hello [ERROR] %d\n",		LOG_LEVEL_ERROR);
+	//LOGC("Hello [CRITICAL] %d\n",	LOG_LEVEL_CRITICAL);
+
 	return 0;
 }
--- etrace/lib/logging/include/log.h
+++ etrace/lib/logging/include/log.h
@@ -17,4 +17,19 @@ void log_write(log_level level, const char *format, ...);
 void log_set_verbosity(log_level level);
 log_level str2loglevel(const char *str, int *ok);
 
+/* Bladerf compatible macros (too much to type though) */
+#define log_verbose(...)	log_write(LOG_LEVEL_VERBOSE, __VA_ARGS__)
+#define log_debug(...)		log_write(LOG_LEVEL_DEBUG, __VA_ARGS__)
+#define log_info(...)		log_write(LOG_LEVEL_INFO, __VA_ARGS__)
+#define log_warning(...)	log_write(LOG_LEVEL_WARNING, __VA_ARGS__)
+#define log_error(...)		log_write(LOG_LEVEL_ERROR, __VA_ARGS__)
+#define log_critical(...)	log_write(LOG_LEVEL_CRITICAL, __VA_ARGS__)
+
+/* Android translation of the macros above (smoother)*/
+#define LOGV log_verbose
+#define LOGD log_debug
+#define LOGI log_info
+#define LOGW log_warning
+#define LOGE log_error
+
 #endif //log_h
