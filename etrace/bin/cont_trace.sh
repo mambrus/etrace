8b02c24515509414d6480c699acf4316f809971f
Michael Ambrus <michael.ambrus@sonymobile.com>
2015-03-11 18:22:39 +0100

    test-script added

---
+++ etrace/bin/cont_trace.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+function maxof() {
+    for P in $(ps -Al | \
+        grep $1 | \
+        grep -v sandbox | \
+        grep -v defunct | \
+        awk '{print $4}'); \
+        do \
+            echo -n "$P "; \
+            pstree -p $P | \
+                grep '{.*}' | \
+                wc -l ; \
+    done | sort -n -k2 | \
+        tac | \
+        head -n1 | \
+        cut -f1 -d" "
+}
+
+function _mytrace() {
+	sudo etrace -p$(maxof $1) -t \
+		-e sched/sched_wakeup -f 'common_pid == %tid%' \
+		-e sched/sched_switch -f 'common_pid == %tid%' \
+		-verror \
+		-T$2 | \
+			grep -vE '^#'
+}
+
+function mytrace() {
+	sudo etrace -p$(maxof $1) -t \
+		-e sched/sched_kthread_stop -f 'common_pid == %tid%' \
+		-e sched/sched_kthread_stop_ret -f 'common_pid == %tid%' \
+		-e sched/sched_migrate_task -f 'common_pid == %tid%' \
+		-e sched/sched_pi_setprio -f 'common_pid == %tid%' \
+		-e sched/sched_process_exec -f 'common_pid == %tid%' \
+		-e sched/sched_process_exit -f 'common_pid == %tid%' \
+		-e sched/sched_process_fork -f 'common_pid == %tid%' \
+		-e sched/sched_process_free -f 'common_pid == %tid%' \
+		-e sched/sched_process_wait -f 'common_pid == %tid%' \
+		-e sched/sched_stat_blocked -f 'common_pid == %tid%' \
+		-e sched/sched_stat_iowait -f 'common_pid == %tid%' \
+		-e sched/sched_stat_runtime -f 'common_pid == %tid%' \
+		-e sched/sched_stat_sleep -f 'common_pid == %tid%' \
+		-e sched/sched_stat_wait -f 'common_pid == %tid%' \
+		-e sched/sched_switch -f 'common_pid == %tid%' \
+		-e sched/sched_wait_task -f 'common_pid == %tid%' \
+		-e sched/sched_wakeup -f 'common_pid == %tid%' \
+		-verror \
+		-T$2 | \
+			grep -vE '^#'
+}
+
+function forever_etrace() {
+	for ((;1;)); do
+		mytrace $1 $2
+	done
+}
+
+forever_etrace "${@}"
+
+#To unset if sourced: 
+# for D in $(cat ../bin/cont_trace.sh | grep function | cut -f2 -d" " | \
+#   sed -e 's/()//');do echo $D; unset $D;
+# done
+
